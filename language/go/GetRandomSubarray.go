76c39cce9a1b2f945670bc7267f12ddd9aa2ecfe
Jachin Huang <hcsxiaohan@gmail.com>
2020-02-07 11:42:21 +0800

    get random subarray

--- language/go/GetRandomSubarray.go
+++ language/go/GetRandomSubarray.go
@@ -1,13 +1,28 @@
-func GetRandomSubarray(candidates []*common.Item, length int32) []*common.Item {
-	result := make([]*common.Item, 0, length)
-	if len(candidates) == 0 {
-		return result
-	}
-	if len(candidates) <= int(length) {
-		return candidates
-	}
-	rand.Shuffle(len(candidates), func(i, j int) {
-		candidates[i], candidates[j] = candidates[j], candidates[i]
-	})
-	return candidates[:int(length)]
+func GetRandomSubarrayInt64(candidates []int64, length int) []int64 {
+    result := make([]int64, 0, length)
+    if len(candidates) == 0 {
+        return result
+    }
+    if len(candidates) <= length {
+        return candidates
+    }
+    rand.Shuffle(len(candidates), func(i, j int) {
+        candidates[i], candidates[j] = candidates[j], candidates[i]
+    })
+    return candidates[:length]
+}
+
+
+func GetRandomSubarrayString(candidates []string, length int) []string {
+    result := make([]string, 0, length)
+    if len(candidates) == 0 {
+        return result
+    }
+    if len(candidates) <= length {
+        return candidates
+    }
+    rand.Shuffle(len(candidates), func(i, j int) {
+        candidates[i], candidates[j] = candidates[j], candidates[i]
+    })
+    return candidates[:length]
 }
