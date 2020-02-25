5127607d03859b3569d392f7a1471d14ac462624
Jachin Huang <hcsxiaohan@gmail.com>
2020-02-25 18:15:11 +0800

    strcpy

---
+++ language/c/strcpy.c
@@ -0,0 +1,17 @@
+char *strcpy(char *des, char *src) {
+    if (des == NULL || src == NULL)
+        return NULL;
+    if (des == src) return des;
+    char *ptr = des;
+    while ((*des++ = *src++) != '\0');
+    return ptr;
+}
+
+char *strncpy(char *des, char *src, size_t n) {
+    if (des == NULL || src == NULL)
+        return NULL;
+    if (des == src) return des;
+    char *ptr = des;
+    while ((n-- > 0) && (*des++ = *src++) != '\0');
+    return ptr;
+}
