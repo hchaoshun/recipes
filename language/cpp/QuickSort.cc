ebe51a5490a26f72089caf07a66e484f0e121e18
Jachin Huang <hcsxiaohan@gmail.com>
2020-04-07 13:52:59 +0800

    quicksort

--- language/cpp/QuickSort.cc
+++ language/cpp/QuickSort.cc
@@ -1,3 +1,8 @@
+//最好情况，每次划分都平均，pivot总是在中央,T(n) = 2*T(n/2) + n => T(n) = nlog(n);
+//最坏情况，初始序列已经有序，第1趟排序经过n-1次比较后，将第1个元素仍然定在原来的位置上，并得到一个长度为n-1的子序列；
+//第2趟排序经过n-2次比较后，将第2个元素确定在它原来的位置上，又得到一个长度为n-2的子序列；以此类推，最终总的比较次数：
+//C(n) = (n-1) + (n-2) + ... + 1 = n(n-1)/2
+//最坏的情况下，快速排序的时间复杂度为O(n^2)
 class Solution {
 public:
     template <typename T>
@@ -10,7 +15,8 @@ public:
 
     template <typename T>
     int partition(vector<T> &vec, int lo, int hi) {
-        swap(vec[lo], vec[lo + rand()%(hi - lo) - 1]);
+        //避免最坏情况发生
+        swap(vec[lo], vec[lo + rand()%(hi - lo + 1)]);
         T pivot = vec[lo];
         while (lo < hi) {
             while (lo < hi && pivot <= vec[hi]) hi--;
